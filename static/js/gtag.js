eb9c7b80a05c58b102ba10855d0585d7a478f65d
Gerardo Gallegos <puxerop@gmail.com>
2018-12-04 09:39:49 -0600

    Se agrega pixel para diferentes eventos

--- static/js/gtag.js
+++ static/js/gtag.js
@@ -6,16 +6,13 @@ gtag('js', new Date())
 gtag('config', 'UA-129457225-1')
 
 // Facebook Pixel
-!(function (f, b, e, v, n, t, s) {
-  if (f.fbq) return; n = f.fbq = function () {
-    n.callMethod
-      ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-  }
-  if (!f._fbq)f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'
-  n.queue = []; t = b.createElement(e); t.async = !0
-  t.src = v; s = b.getElementsByTagName(e)[0]
-  s.parentNode.insertBefore(t, s)
-}(window, document, 'script',
-  'https://connect.facebook.net/en_US/fbevents.js'))
-fbq('init', '935365103327627')
+!(function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js'));
+fbq('init', '1936544896381515')
 fbq('track', 'PageView')
